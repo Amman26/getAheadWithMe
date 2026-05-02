b1b418550281f5125680a6f92af3a99b7277dde6
Amman Farooque <129602205+Amman26@users.noreply.github.com>
2026-05-02 10:15:17 +0530

    Create day7src.py

---
+++ day7src.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+
+def to_upper(text):
+    return text.upper()
+
+
+def get_numbers():
+    return [1, 2, 3]
+
+
+def square(x):
+    return x * x
+
+
+def divide(a, b):
+    return a / b
