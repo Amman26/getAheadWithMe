7b0e5684a99109f9607827cb919e89f5227663dd
Amman Farooque <129602205+Amman26@users.noreply.github.com>
2026-05-02 10:57:58 +0530

    Create day8 src.py

---
+++ day8 src.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def get_list():
+    return [1, 2, 3]
+
+def to_upper(text):
+    return text.upper()
+
+def is_upper(text):
+    return text.isupper()
+
+def divide(a, b):
+    return a / b
