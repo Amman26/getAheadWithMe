90b8ab9d8c5002c0644fc7f98d5ec19682046662
Amman Farooque <129602205+Amman26@users.noreply.github.com>
2026-05-07 21:00:07 +0530

    Update day9.py

--- day9.py
+++ day9.py
@@ -7,6 +7,17 @@ def test_math_operations():
 
     # Check if "pytest" is present in the sentence
     assert "pytest" in "Learning pytest is fun"
+    PS C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9> pytest -v
+============================================================================= test session starts ==============================================================================
+platform win32 -- Python 3.14.4, pytest-9.0.3, pluggy-1.6.0 -- C:\Program Files\Python314\python.exe
+cachedir: .pytest_cache
+rootdir: C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9
+collected 1 item                                                                                                                                                                
+
+test_logic.py::test_math_operations PASSED                                                                                                                                [100%]
+
+============================================================================== 1 passed in 0.06s ===============================================================================
+PS C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9> 
   2.
 import pytest
 
@@ -22,6 +33,17 @@ def test_dict_keys(sample_dict):
 
     # Check value of "name"
     assert sample_dict["name"] == "Alice"
+    PS C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9> pytest -v test_fixture.py        
+============================================================================= test session starts ==============================================================================
+platform win32 -- Python 3.14.4, pytest-9.0.3, pluggy-1.6.0 -- C:\Program Files\Python314\python.exe
+cachedir: .pytest_cache
+rootdir: C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9
+collected 1 item                                                                                                                                                                
+
+test_fixture.py::test_dict_keys PASSED                                                                                                                                    [100%]
+
+============================================================================== 1 passed in 0.02s ===============================================================================
+PS C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9> 
   3.
 import pytest
 
@@ -33,6 +55,22 @@ def get_element(my_list, index):
 def test_index_error():
     with pytest.raises(IndexError):
         get_element([1, 2, 3], 10)
+        Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
+
+PS C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9> pytest -v test_exception.py
+============================================================================= test session starts ==============================================================================
+platform win32 -- Python 3.14.4, pytest-9.0.3, pluggy-1.6.0 -- C:\Program Files\Python314\python.exe
+cachedir: .pytest_cache
+rootdir: C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9
+collected 1 item                                                                                                                                                                
+
+test_exception.py::test_index_error PASSED                                                                                                                                [100%]
+
+============================================================================== 1 passed in 0.03s ===============================================================================
+
 4.
 import pytest
 
@@ -40,6 +78,19 @@ import pytest
 @pytest.mark.parametrize("number", [2, 10, 22])
 def test_is_even(number):
     assert number % 2 == 0
+PS C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9> pytest -v test_parametrize.py
+============================================================================= test session starts ==============================================================================
+platform win32 -- Python 3.14.4, pytest-9.0.3, pluggy-1.6.0 -- C:\Program Files\Python314\python.exe
+cachedir: .pytest_cache
+rootdir: C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9
+collected 3 items                                                                                                                                                               
+
+test_parametrize.py::test_is_even[2] PASSED                                                                                                                               [ 33%]
+test_parametrize.py::test_is_even[10] PASSED                                                                                                                              [ 66%]
+test_parametrize.py::test_is_even[22] PASSED                                                                                                                              [100%]
+
+============================================================================== 3 passed in 0.02s ===============================================================================
+PS C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9> 
 5.
 import pytest
 import os
@@ -65,6 +116,18 @@ def test_file_content(temp_file):
         content = file.read()
 
     assert content == "Hello World"
+
+PS C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9> pytest -v test_file.py
+============================================================================= test session starts ==============================================================================
+platform win32 -- Python 3.14.4, pytest-9.0.3, pluggy-1.6.0 -- C:\Program Files\Python314\python.exe
+cachedir: .pytest_cache
+rootdir: C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9
+collected 1 item                                                                                                                                                                
+
+test_file.py::test_file_content PASSED                                                                                                                                    [100%]
+
+============================================================================== 1 passed in 0.03s ===============================================================================
+PS C:\Wipro Training\Python\PythonCoding\Daily Assignments\Day 9> 
 
   Before the test runs:
 test.txt is created.
